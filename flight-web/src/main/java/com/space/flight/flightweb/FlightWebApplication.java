216cf760e406ec6cf0c462d70a0044374a9db55f
egor.sumskoy <segoruch@gmail.com>
2022-11-22 03:58:16 +0200

    flight-web inited

---
+++ flight-web/src/main/java/com/space/flight/flightweb/FlightWebApplication.java
@@ -0,0 +1,13 @@
+package com.space.flight.flightweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightWebApplication.class, args);
+	}
+
+}
---
+++ flight-web/src/test/java/com/space/flight/flightweb/FlightWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.space.flight.flightweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
